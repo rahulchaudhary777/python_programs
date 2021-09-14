3492c63f18ca09a77ff68428720e49ed6d078cbc
Rahul <ch.rahuljaat777@gmail.com>
2021-09-15 01:35:04 +0530

    remove error

--- basic_programs/ATM_100_note_compulsory.py
+++ basic_programs/ATM_100_note_compulsory.py
@@ -1,9 +1,9 @@
 a = int(input('enter the amount '))
 a = a-100
 b = int(a/2000)
-a = a%2000
+a = a % 2000
 c = int(a/500)
-a = a%500
+a = a % 500
 d = int(a/100)
-a = a%100
-print('notes of 2000 & 500 & 100 & reminder amount are ', b,c,d+1,a)
+a = a % 100
+print('notes of 2000 & 500 & 100 & reminder amount are ', b, c, d+1, a)
